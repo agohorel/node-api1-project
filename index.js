544e82581de4bdb18a8b9c6c461ddc5c8056a493
agohorel <gohorelalex@gmail.com>
2020-02-10 10:43:42 -0500

    add POST /api/users route

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const db = require("./data/db");
 const port = 8000;
 
 const server = express();
+server.use(express.json());
 
 server.get("/", (req, res) => {
   res.send("hello world");
+});
+
+// POST api/users
+server.post("/api/users", async (req, res) => {
+  if (!req.body.name || !req.body.bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user" });
+  }
+  try {
+    const newEntryID = await db.insert(req.body);
+    const newEntry = await db.findById(newEntryID.id);
+    res.status(201).json(newEntry);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      errorMessage: "There was an error while saving the user to the database."
+    });
+  }
 });
 
 server.listen(port, () => console.log(`server listening on port ${port}`));
